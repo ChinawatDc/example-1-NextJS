e4f01c285801b7a831abd2c4a28e5f26f9bf959b
ChinawatDc <chinawat.dc@gmail.com>
2024-08-13 14:43:15 +0700

    [Debug]

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useEffect, useState } from "react"
 import { TabPanel, TabView } from "primereact/tabview";
 import { getBarBasics, getBarChart } from "@/services/api/api.mockup";
